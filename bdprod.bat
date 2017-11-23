31954eeecb15ebc2dacdee2b693e8f452c8ebc4d
ricardo <rickvanni@gmail.com>
2017-11-23 20:54:51 -0200

    scripts bath para windows - rodar bd teste e prod

---
+++ bdprod.bat
@@ -0,0 +1,3 @@
+@echo off
+cd TrilhaJEESQL-ejb\src\main\resources\lancamento
+java -cp hsqldb-2.4.0.jar org.hsqldb.Server -database.0 lancamento -dbname.0 lancamento
---
+++ bdtest.bat
@@ -0,0 +1,3 @@
+@echo off
+cd TrilhaJEESQL-ejb\src\test\resources\lancamento
+java -cp hsqldb-2.4.0.jar org.hsqldb.Server -database.0 lancamento -dbname.0 lancamento
